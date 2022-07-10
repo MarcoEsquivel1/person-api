545e9e37401ef2d9e16174fb8e20ccdca8e92270
MarcoEsquivel <marco.esquivel@catolica.edu.sv>
2022-07-10 17:51:44 -0600

    crud

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -34,7 +34,29 @@ class PersonController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $jsonPerson = $request->json()->all();
+
+        $person = new Person($jsonPerson);
+
+        /* $person->firstName = $jsonPerson['firstName'];
+        $person->lastName = $jsonPerson['lastName'];
+        $person->documentNumber = $jsonPerson['documentNumber'];
+        $person->country = $jsonPerson['country'];
+        $person->city = $jsonPerson['city'];
+        $person->street = $jsonPerson['street'];
+        $person->number = $jsonPerson['number'];
+        $person->single = $jsonPerson['single']; */
+
+        $person->save();
+
+        $res = [
+            "status" => 'ok',
+            "message" => 'Persona creada',
+            "code" => 1003,
+            "data" => $person
+        ];
+
+        return $res;
     }
 
     /**
@@ -76,7 +98,25 @@ class PersonController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $person = Person::find($id);
+
+        if(isset($person)){
+            $person->update($request->json()->all());
+
+            $res = [
+                "status" => 'ok',
+                "message" => 'Persona actualizada',
+                "code" => 1005
+            ];
+        }else{
+            $res = [
+                "status" => 'fail',
+                "message" => 'No se encontro la persona a actualizar',
+                "code" => 1015
+            ];
+        }
+
+        return $res;
     }
 
     /**
@@ -87,6 +127,25 @@ class PersonController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $person = Person::find($id);
+
+        if(isset($person)){
+            $person->delete();
+
+            $res = [
+                "status" => 'ok',
+                "message" => 'Persona eliminada',
+                "code" => 1004
+            ];
+        }else{
+            $res = [
+                "status" => 'fail',
+                "message" => 'No se encontro la persona a eliminar',
+                "code" => 1014
+            ];
+        }
+
+        return $res;
+
     }
 }
--- app/Models/Person.php
+++ app/Models/Person.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Person extends Model
 {
     protected $table = "persons";
+
+    protected $fillable = [
+        "firstName", "lastName", "documentNumber", "country", 
+        "city", "street", "number", "single"
+    ];
+
+    protected $hidden = [
+        "created_at", "updated_at"
+    ];
 }
